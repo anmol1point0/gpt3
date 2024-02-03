8f327cd2baacc579215e8faefde6a729a34b1d46
Anmol Mittal <anmol.mittal@jumbotail.com>
2024-02-04 02:36:44 +0530

    Added Brands and whatgpt3

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './brand.css';
-
+import { google, shopify, slack, atlassian, dropbox } from './imports';
 const Brand = () => {
   return (
-    <div>
-        Brand
+    <div className='gpt3__brand section_padding'>
+
+      <div className='gpt3__brand-google'>
+        <img src = {google} alt='google'/>
+      </div>
+
+      <div className='gpt3__brand-slack'>
+        <img src = {slack} alt='slack'/>
+      </div>
+
+      <div className='gpt3__brand-atlassian'>
+        <img src = {atlassian} alt='atlassian'/>
+      </div>
+
+      <div className='gpt3__brand-dropbox'>
+        <img src = {dropbox} alt='dropbox'/>
+      </div>
+
+      <div className='gpt3__brand-shopify'>
+        <img src = {shopify} alt='shopify'/>
+      </div> 
+
     </div>
   )
 }
