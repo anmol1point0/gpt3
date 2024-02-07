48ada23d89f536316ae9fa61f59b968eeb794010
Anmol Mittal <anmol.mittal@jumbotail.com>
2024-02-07 18:45:16 +0530

    Added blogs

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imageUrl, date, title}) => {
   return (
-    <div>
-        Article
+    <div className='gpt3__article'>
+        <div className='gpt3__article-img'>
+          <img src={imageUrl} alt='article'/>
+        </div>
+        <div className='gpt3__article-content'>
+          <p>{date}</p>
+          <h3>{title}</h3>
+          <div>
+            <p>Read Full Article</p>
+          </div>
+        </div>
     </div>
   )
 }
